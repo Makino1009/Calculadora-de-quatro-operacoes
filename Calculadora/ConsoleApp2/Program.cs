ccdd2216a581d1f8b44d8d2905e97a1a2aa637ff
Makino1009 <46648457+Makino1009@users.noreply.github.com>
2019-03-12 19:51:10 +0000

    Add files via upload

---
+++ Calculadora/ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            float a, b, r; 
+            string aa;
+
+
+            Console.WriteLine("Insira o primeiro valor: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o segundo valor: ");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("O que quer fazer?");
+            aa = (Console.ReadLine());
+            switch (aa)
+            {
+                case
+                "somar":
+                    r = a + b; Console.WriteLine(r);
+ break;
+                case
+                "subtrair":
+                    r = a - b; Console.WriteLine(r);
+ break;
+                case
+                    "multiplicar":
+                    r = a * b; Console.WriteLine(r);
+ break;
+                case
+                    "dividir":
+                    r = a / b; Console.WriteLine(r);
+ break;          
+                
+                default: Console.WriteLine("favor escrever uma operação reconhecida."); break;
+            }
+            Console.WriteLine("Aperte enter pra fechar o programa.");
+            Console.ReadLine();
+
+        }
+    }
+}
